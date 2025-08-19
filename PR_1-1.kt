3d6c6ef5ad1680ba9d0339fb3b131c4505320625
BM_INFINITY <126513083+BM-INFINITY@users.noreply.github.com>
2025-08-19 09:23:53 +0530

    Add files via upload

---
+++ PR_1-1.kt
@@ -0,0 +1,22 @@
+import java.security.KeyStore.TrustedCertificateEntry
+
+fun main(){
+    var a:Int=10
+    var b:Float=1.5F
+    var c:Char='h'
+    var d:String="hello"
+    var e:Boolean=true
+    var f:Double=96.36
+    var g:Long=22222222
+    var h:Short=2
+    var i:Byte=11
+    println("Integer Value=$a")
+    println("Float Value=$b")
+    println("Char Value=$c")
+    println("String Value=$d")
+    println("boolean Value=$e")
+    println("double Value=$f")
+    println("long Value=$g")
+    println("short Value=$h")
+    println("Byte Value=$i")
+}
